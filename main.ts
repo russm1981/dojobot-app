a32dd364e6c8aee46d46a8aab9f960d9476f2bfb
russm1981 <145505378+russm1981@users.noreply.github.com>
2024-01-03 23:14:28 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    dojobot.bot_servo(LEFT, 0)
+    dojobot.bot_servo(RIGHT, 0)
+    dojobot.bot_servo(ROTATE, 0)
+    dojobot.bot_servo(JAW1, 0)
+    dojobot.bot_servo(JAW2, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    dojobot.bot_relay(1)
+})
+input.onButtonPressed(Button.B, function () {
+    dojobot.bot_servo(LEFT, 180)
+    dojobot.bot_servo(RIGHT, 180)
+    dojobot.bot_servo(ROTATE, 180)
+    dojobot.bot_servo(JAW1, 180)
+    dojobot.bot_servo(JAW2, 180)
+})
+let inJOYL_X = 0
+let inJOYL_Y = 0
+let inJOYR_X = 0
+let inJOYR_Y = 0
+let inSLIDE = 0
+let inVER = 0
+let inKNOB = 0
+
+let JAW2 = 0
+let JAW1 = 0
+let ROTATE = 0
+let RIGHT = 0
+let LEFT = 0
 basic.showLeds(`
     . . . . .
     . . . . .
@@ -5,62 +38,39 @@ basic.showLeds(`
     . . . . .
     # # # # #
     `)
+serial.redirect(
+SerialPin.USB_TX,
+SerialPin.USB_RX,
+BaudRate.BaudRate9600
+)
+serial.writeString("dojo:bot App v1")
 dojobot.bot_init()
+dojobot.bot_led_colour(1, 14548991)
+dojobot.bot_led_colour(2, 16777215)
+dojobot.bot_led_colour(3, 16777215)
+LEFT = 6
+RIGHT = 0
+ROTATE = 1
+JAW1 = 5
+JAW2 = 4
+
+const ADC_CH_LEFTJOY_Y = 0
+const ADC_CH_LEFTJOY_X = 1
+const ADC_CH_SLIDE = 2
+const ADC_CH_EXPANS = 3
+const ADC_CH_RIGHTJOY_Y = 4
+const ADC_CH_RIGHTJOY_X = 5
+const ADC_CH_KNOB = 6
+const ADC_CH_VERSION = 7
+
 basic.forever(function () {
-    // if (dojobot.bot_input(0) < 2000) {
-    // led.plot(0, 0)
-    // } else {
-    // led.unplot(0, 0)
-    // }
-    // if (dojobot.bot_input(1) < 2000) {
-    // led.plot(0, 1)
-    // } else {
-    // led.unplot(0, 1)
-    // }
-    // if (dojobot.bot_input(2) < 2000) {
-    // led.plot(0, 2)
-    // } else {
-    // led.unplot(0, 2)
-    // }
-    // if (dojobot.bot_input(3) < 2000) {
-    // led.plot(0, 3)
-    // } else {
-    // led.unplot(0, 3)
-    // }
-    // if (dojobot.bot_input(4) < 2000) {
-    // led.plot(1, 0)
-    // } else {
-    // led.unplot(1, 0)
-    // }
-    // if (dojobot.bot_input(5) < 2000) {
-    // led.plot(1, 1)
-    // } else {
-    // led.unplot(1, 1)
-    // }
-    // if (dojobot.bot_input(6) < 2000) {
-    // led.plot(1, 2)
-    // } else {
-    // led.unplot(1, 2)
-    // }
-    // if (dojobot.bot_input(7) < 2000) {
-    // led.plot(1, 3)
-    // } else {
-    // led.unplot(1, 3)
-    // }
-    led.plotBarGraph(
-    dojobot.bot_input(2),
-    100,
-    true
-    )
-    pins.digitalWritePin(DigitalPin.P9, 1)
-    if (input.buttonIsPressed(Button.A)) {
-        led.plot(4, 4)
-    } else {
-        led.unplot(4, 4)
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        led.plot(4, 3)
-    } else {
-        led.unplot(4, 3)
-    }
+    dojobot.bot_relay(0)
+    inJOYL_X = dojobot.bot_input(ADC_CH_LEFTJOY_X)
+    inJOYL_Y = dojobot.bot_input(ADC_CH_LEFTJOY_Y)
+    inJOYR_X = dojobot.bot_input(ADC_CH_RIGHTJOY_X)
+    inJOYR_Y = dojobot.bot_input(ADC_CH_RIGHTJOY_Y)
+    inSLIDE = dojobot.bot_input(ADC_CH_SLIDE)
+    inKNOB = dojobot.bot_input(ADC_CH_KNOB)
+    inVER = dojobot.bot_input(ADC_CH_VERSION)
+    serial.writeLinewriteline(`OP LX${inJOYL_X} LY${inJOYL_Y} RX${inJOYR_X} RY${inJOYR_Y} S${inSLIDE} K${inKNOB} V${inVER}`)
 })
