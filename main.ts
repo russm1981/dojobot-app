999e20c4384f5084614716ebb1b4f2448572a3be
russm1981 <145505378+russm1981@users.noreply.github.com>
2024-02-02 21:49:10 +0000

    app4

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
+
+let inputval = 0
 let MODE = 0
+/*
+let S_RIGHT = 0
 let COLOUR = 0
 let inKNOB = 0
 let inVER = 0
@@ -7,38 +11,54 @@ let inJOYR_Y = 0
 let inJOYR_X = 0
 let inJOYL_Y = 0
 let inJOYL_X = 0
-const S_LEFT = 6
-const S_RIGHT = 0
-const S_ROTATE = 1
-const S_JAW1 = 5
-const S_JAW2 = 4
-const A_LEFTJOY_Y = 0
-const A_LEFTJOY_X = 1
-const A_SLIDE = 2
-const A_EXPANS = 3
-const A_RIGHTJOY_Y = 4
-const A_RIGHTJOY_X = 5
-const A_KNOB = 6
-const A_VERSION = 7
+let degrees_calc2 = 0
+let degrees_calc = 0
+let amount_to_move = 0
+let S_LEFT = 6
+let S_ROTATE = 1
+let S_JAW1 = 5
+let S_JAW2 = 4
+let A_LEFTJOY_X = 1
+let A_SLIDE = 2
+let A_EXPANS = 3
+let A_RIGHTJOY_Y = 4
+let A_RIGHTJOY_X = 5
+let A_KNOB = 6
+let A_VERSION = 7
+let LEFT_POSITION = 0
+let RIGHT_POSITION = 0
+let ROTATE_POSITION = 0
+*/
 serial.redirect(
 SerialPin.USB_TX,
 SerialPin.USB_RX,
 BaudRate.BaudRate9600
 )
-serial.writeString("Welcome to dojo:bot App v1")
-dojobot.bot_init()
+serial.writeString("Welcome to dojo:bot App v4.0")
+dojo.bot_init()
+dojo.bot_servo(S_LEFT, 90)
+dojo.bot_servo(S_RIGHT, 90)
+dojo.bot_servo(S_ROTATE, 90)
+dojo.bot_servo(S_JAW1, 90)
+dojo.bot_servo(S_JAW2, 90)
 if (input.buttonIsPressed(Button.A)) {
     basic.showIcon(IconNames.Square)
     music.play(music.builtinPlayableSoundEffect(soundExpression.slide), music.PlaybackMode.UntilDone)
     MODE = 0
+    dojo.bot_led_colour(1, 16764159)
+    dojo.bot_led_colour(2, 16777215)
+    dojo.bot_led_colour(3, 16764159)
 } else {
+    basic.showString("v4.0")
     basic.showIcon(IconNames.Heart)
     music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
     MODE = 1
+    dojobot.bot_led_colour(1, 13434879)
+    dojobot.bot_led_colour(2, 16777215)
+    dojobot.bot_led_colour(3, 13434879)
 }
 basic.forever(function () {
     if (MODE == 0) {
-
         // In servo calibration mode
         // On A -> send to 0
         if (pins.digitalReadPin(DigitalPin.P5) == 0) {
@@ -73,7 +93,7 @@ basic.forever(function () {
             }
         }
     } else {
-        
+        let A_LEFTJOY_Y = 0
         // In normal operation
         // Transmit joystick functions onwards
         inJOYL_X = dojobot.bot_input(A_LEFTJOY_X)
@@ -84,5 +104,77 @@ basic.forever(function () {
         inKNOB = dojobot.bot_input(A_KNOB)
         inVER = dojobot.bot_input(A_VERSION)
         serial.writeLine("" + (`OP LX${inJOYL_X} LY${inJOYL_Y} RX${inJOYR_X} RY${inJOYR_Y} S${inSLIDE} K${inKNOB} V${inVER}`))
+        inputval = inJOYL_Y
+        //Joystick value is 0 to approx 4096, with 2200 being central point 
+        if (inputval > 3500) amount_to_move = -3;
+        else if (inputval > 3500) amount_to_move = -3;
+        else if (inputval > 3000) amount_to_move = -2;
+        else if (inputval > 2300) amount_to_move = -1;
+        else if (inputval < 550) amount_to_move = 3;
+        else if (inputval < 1100) amount_to_move = 2;
+        else if (inputval < 2100) amount_to_move = 1;
+        else amount_to_move = 0;
+        
+        dojobot.bot_servo(S_LEFT, degrees_calc)
+        serial.writeLine("" + (`LEFT Y input ${inJOYL_Y} movement ${amount_to_move}`))
+        degrees_calc = 0
+        inputval = inJOYR_Y
+        if (inputval > 0) {
+            if (inputval < 100) {
+                degrees_calc = 90
+            } else {
+                inputval += 0 - 100
+                degrees_calc = Math.round(90 - inputval / 20)
+                Math.constrain(degrees_calc, 0, 90)
+            }
+        } else {
+            inputval *= -1
+if (inputval < 100) {
+                degrees_calc = 90
+            } else {
+                inputval += 0 - 100
+                degrees_calc = Math.round(90 + inputval / 23.4)
+                Math.constrain(degrees_calc, 90, 180)
+            }
+        }
+        dojobot.bot_servo(S_RIGHT, degrees_calc)
+        serial.writeLine("" + (`RIGHT Y input ${inJOYR_Y} degrees ${degrees_calc}`))
+        degrees_calc = 0
+        inputval = inJOYL_X
+        if (inputval > 0) {
+            if (inputval < 100) {
+                degrees_calc = 90
+            } else {
+                inputval += 0 - 100
+                degrees_calc = Math.round(90 - inputval / 20)
+                Math.constrain(degrees_calc, 0, 90)
+            }
+        } else {
+            inputval *= -1
+if (inputval < 100) {
+                degrees_calc = 90
+            } else {
+                inputval += 0 - 100
+                degrees_calc = Math.round(90 + inputval / 23.4)
+                Math.constrain(degrees_calc, 90, 180)
+            }
+        }
+        dojobot.bot_servo(S_ROTATE, degrees_calc)
+        serial.writeLine("" + (`ROTATE input ${inJOYL_X} degrees ${degrees_calc}`))
+        degrees_calc = 0
+        inputval = inSLIDE
+        if (inputval < 100) {
+            degrees_calc = 60
+            degrees_calc2 = 120
+        } else {
+            inputval += 0 - 100
+            degrees_calc = Math.round(80 + inputval / 50)
+            degrees_calc2 = Math.round(100 - inputval / 50)
+            Math.constrain(degrees_calc, 0, 180)
+Math.constrain(degrees_calc2, 0, 180)
+        }
+        dojobot.bot_servo(S_JAW1, degrees_calc2)
+        dojobot.bot_servo(S_JAW2, degrees_calc)
+        serial.writeLine("" + (`JAWS input ${inSLIDE} degrees1 ${degrees_calc} degrees2 ${degrees_calc2}`))
     }
 })
